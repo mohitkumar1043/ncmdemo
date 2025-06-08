e46d63f7c29ba552c473cf7edbef022ba9d6aad2
Mohit Kumar <121260020+mohitkumar1043@users.noreply.github.com>
2025-06-08 14:10:15 +0530

    Update DBConnection.java

--- NCM/src/main/java/DBConnection/DBConnection.java
+++ NCM/src/main/java/DBConnection/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-	private static final String URL = "jdbc:mysql://localhost:3306/ncmdb";
+	private static final String URL = "";
     private static final String USER = "root";
     private static final String PASS = "";
     public static Connection getConnection() throws SQLException {
